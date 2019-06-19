a63eb207b25b4994b9315e6a19426f8d81e4a9de
Soichiro KATO <kato-soichiro@msen.jp>
2019-06-20 08:33:40 +0900

    インデントを整形

--- src/components/HttpCatComponent.vue
+++ src/components/HttpCatComponent.vue
@@ -1,33 +1,35 @@
 <template>
+  <div>
     <div>
-        <div>
-            <h2>{{ message }}:</h2>
-            <select v-model="status">
-                <option v-for="item in statusList" :key="item.code" :value="item.code">
-                    {{ item.code }} - {{ item.message }}
-                </option>
-            </select>
-        </div>
-        <div>
-            <img :src="`https://http.cat/${status}`">
-        </div>
+      <h2>{{ message }}:</h2>
+      <select v-model="status">
+        <option
+          v-for="item in statusList"
+          :key="item.code"
+          :value="item.code"
+        >{{ item.code }} - {{ item.message }}</option>
+      </select>
     </div>
+    <div>
+      <img :src="`https://http.cat/${status}`">
+    </div>
+  </div>
 </template>
 
 <script>
-import http from '../http.js'
+import http from "../http.js";
 export default {
-    data() {
-        return {
-            status: 200,
-            statusList: http.statusList
-        }
-    },
+  data() {
+    return {
+      status: 200,
+      statusList: http.statusList
+    };
+  },
 
-    computed: {
-        message() {
-            return this.statusList.find(item => item.code === this.status).message
-        }
-    },
-}
+  computed: {
+    message() {
+      return this.statusList.find(item => item.code === this.status).message;
+    }
+  }
+};
 </script>
